6e25271eae373316dfa6a91b63d4cba65d76d008
MrTux <mrtux@ubuntu-vn.org>
2012-05-22 09:32:31 +0700

    rake db:migrate

--- db/migrate/20120522022740_create_articles.rb
+++ db/migrate/20120522022740_create_articles.rb
@@ -1,10 +1,14 @@
 class CreateArticles < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :articles do |t|
       t.string :title
       t.text :body
 
       t.timestamps
     end
+  end
+
+  def self.down
+    drop_table :articles
   end
 end

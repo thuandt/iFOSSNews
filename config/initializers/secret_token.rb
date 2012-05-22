9d16e318bf06705968a31d74b6e71741d856f457
MrTux <mrtux@ubuntu-vn.org>
2012-05-22 08:00:21 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IFOSSNews::Application.config.secret_token = '7ee2ccd37bf7460ecc624745b3afa95b8dc93084cc6bd44de2e09c688d446427ed08e8e5a9aed9045da73a72b4f992f902167f9be2a460d6d3aa920f7ef2ff97'
